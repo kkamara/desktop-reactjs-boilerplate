5aab87005c745a35ee457effc383fe271d1e343a
kkamara <kamaracomputers@gmail.com>
2023-06-01 15:19:04 +0100

    Update serving react for react 18

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot, } from 'react-dom/client'
 
-ReactDOM.render(
-  <h1>Hello React App</h1>,
-  document.getElementById('root'),
+import Main from './Main'
+import './index.scss'
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(
+  <React.StrictMode>
+    <Main />,
+  </React.StrictMode>
 )
