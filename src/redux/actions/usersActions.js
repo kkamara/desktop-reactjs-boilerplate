65da47bff44ede1ce51d3204ee2e08ed088ca9fa
kkamara <kamaracomputers@gmail.com>
2024-11-30 15:22:30 +0000

    Upgrade all packages (like ElectronJS & ReactJS 16 to 18) (needs works on compatibility with ElectronJS)

---
+++ src/redux/actions/usersActions.js
@@ -0,0 +1,27 @@
+
+import HttpService from '../../services/HttpService'
+import { users, } from '../types'
+
+export const getUsers = page => {
+  return async dispatch => {
+    const http = new HttpService()
+
+    dispatch({ type: users.GET_USERS_PENDING, })
+
+    const tokenId = "user-token"
+    const path = page ? '/users/?page='+page : '/users'
+    await new Promise((resolve, reject) => {
+      http.getData(path, tokenId).then(res => {
+        resolve(dispatch({
+          type: users.GET_USERS_SUCCESS,
+          payload: res.data,
+        }))                
+      }, error => {
+        reject(dispatch({ 
+          type : users.GET_USERS_ERROR, 
+          payload: error,
+        }))
+      })
+    })
+  }
+}
