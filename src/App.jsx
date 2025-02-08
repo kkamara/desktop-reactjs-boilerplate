d0a799861adbf051ab131609015b5911c5bfb88a
kkamara <kamaracomputers@gmail.com>
2025-02-08 20:56:07 +0000

    Update indentation to 4 spaces

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { BrowserRouter as Router, } from 'react-router-dom';
 import Routes from "./Routes";
 
 function App() {
-  return (
-    <Router>
-        <Routes/>
-    </Router>
-  )
+    return (
+        <Router>
+            <Routes/>
+        </Router>
+    )
 }
 
 export default App;
--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -4,20 +4,20 @@ import { APP_NAME, } from '../../constants';
 
 export default function Header(props) {
   return <nav className="navbar navbar-expand-lg mb-4 bg-primary" data-bs-theme="dark">
-    <div className="container">
-      <Link className="navbar-brand" to="/">
-        {APP_NAME}
-      </Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-          </li>
-        </ul>
+      <div className="container">
+          <Link className="navbar-brand" to="/">
+              {APP_NAME}
+          </Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                      <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                  </li>
+              </ul>
+          </div>
       </div>
-    </div>
   </nav>
 }
